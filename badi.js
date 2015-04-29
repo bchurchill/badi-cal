79b11c49db3887c2b092351810904cbe6a92b788
Berkeley Churchill (on remembrance) <berkeley@berkeleychurchill.com>
2015-04-28 22:23:37 -0700

    filter days more generously for display to user

--- badi.js
+++ badi.js
@@ -409,7 +409,8 @@ var BadiCal = {
 
     // 5. filter through these to find those that are actually in provided date range
     days = days.filter(function(day) {
-      return (start_date <= day.start_date.addDays(-1) && day.start_date <= end_date);
+      return (start_date <= day.start_date && day.start_date <= end_date ||
+              start_date <= day.end_date && day.end_date <= end_date);
     });
 
     // 6. sort by gregorian date
